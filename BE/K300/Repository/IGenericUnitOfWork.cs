3ab7e88a84ffabfdd5fc24cd5304e3817a4371bc
Ng Thanh Cong <nxgthanhcong@gmail.com>
2021-05-25 10:06:28 +0700

    commit code

---
+++ BE/K300/Repository/IGenericUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public interface IGenericUnitOfWork<T> : IDisposable
+    {
+        IGenericRepository<T> GenericRepository { get; }
+        IBrandRepository BrandRepository { get; }
+        void Commit();
+    }
+}
---
+++ BE/K300/Services/Implementions/BrandService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Models;
+using Repository;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Implementions
+{
+    public class BrandService : IBrandService
+    {
+        private IConfiguration _configuration;
+        private readonly string _connectionString;
+        public BrandService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task<IEnumerable<Brand>> GetAllBrand()
+        {
+            try
+            {
+                using (IUnitOfWork uow = new UnitOfWork(_connectionString))
+                {
+                    var result = await uow.BrandRepository.GetAllBrand();
+                    uow.Commit();
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public async Task<int> AddBrand()
+        {
+            try
+            {
+                using (IUnitOfWork uow = new UnitOfWork(_connectionString))
+                {
+                    var result = await uow.BrandRepository.GetAllBrand();
+                    uow.Commit();
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
